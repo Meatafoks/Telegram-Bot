548eba16b0fbb1872cc44ff18dcb16624c47811a
Яков Фокс <diegoling33@gmail.com>
2024-01-29 23:24:24 +0300

    clean up repository

--- src/__tests___/telegramMessageHandler.component.ts
+++
@@ -1,9 +0,0 @@
-import { NewMessageEvent, TelegramMessageHandler } from '../';
-import { Component } from '@metafoks/app';
-
-@Component('telegramMessageHandler')
-export default class TelegramMessageHandlerComponent implements TelegramMessageHandler {
-    async onMessage(event: NewMessageEvent): Promise<void> {
-        await event.chat.sendMessage('Hi');
-    }
-}
